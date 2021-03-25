244912c6b98a3e28c8a8bda10c71d28723c02b61
Jeremy <reaper9998@hotmail.com>
2021-03-25 15:42:15 -0400

    updated log file name to variable

--- run_script.sh
+++ run_script.sh
@@ -16,7 +16,7 @@ for i in ${brokers[@]}; do
 		sleep 10
 
 		echo "Running script with $t publishers and $5 subscribers with 0.04 messages/second for 15 minutes."
-		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=10 --limitLogging=1 | tee ./reports/hivemq$t.log
+		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=10 --limitLogging=1 | tee ./reports/$i$t.log
 
 		echo "Stopping $i Docker"
 		sudo docker stop $i
