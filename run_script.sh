c434a9b2396dbf3af075b86faa4b9a5de6b3adbe
Jeremy <reaper9998@hotmail.com>
2021-03-25 15:45:24 -0400

    added end message

--- run_script.sh
+++ run_script.sh
@@ -15,10 +15,11 @@ for i in ${brokers[@]}; do
 		echo "Waiting for broker to finish starting up"
 		sleep 10
 
-		echo "Running script with $t publishers and $5 subscribers with 0.04 messages/second for 15 minutes."
+		echo "Running node script with $t publishers and $t subscribers with 0.04 messages/second for 15 minutes."
 		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=10 --limitLogging=1 | tee ./reports/$i$t.log
 
 		echo "Stopping $i Docker"
 		sudo docker stop $i
 	done
 done
+echo "Testing sucessfully completed. Check reports directory for results."
