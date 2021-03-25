5a1bfb0d10b18cd05979724fde7541cba5d1d9d1
Jeremy <reaper9998@hotmail.com>
2021-03-25 16:23:22 -0400

    updated script length to 15 minutes

--- run_script.sh
+++ run_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+rm -r reports
 mkdir -p reports
 
-brokers=("hivemq1" "emqx" "vernemq")
+brokers=("emqx" "hivemq1" "vernemq")
 numClients=("50" "500" "5000" "10000")
 
 echo "Stopping all existing brokers"
@@ -17,10 +17,10 @@ for i in ${brokers[@]}; do
 		sleep 10
 
 		echo "Running node script with $t publishers and $t subscribers with 0.04 messages/second for 15 minutes."
-		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=10 --limitLogging=1 | tee ./reports/$i$t.log
+		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=900 --limitLogging=1 | tee ./reports/$i$t.log
 
 		echo "Stopping $i Docker"
 		sudo docker stop $i
-	done
+	done 
 done
 echo "Testing sucessfully completed. Check reports directory for results."
