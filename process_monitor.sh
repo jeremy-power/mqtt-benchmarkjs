a5f233dde2c6fa181dd6c6c77bb7c11ce8ea9295
Jeremy <reaper9998@hotmail.com>
2021-03-26 13:13:33 -0400

    updated process monitor to exclude self processes

--- process_monitor.sh
+++ process_monitor.sh
@@ -11,7 +11,7 @@ while true; do
         ps axo pid,etime,%cpu,%mem,cmd | grep 'beam.smp' | grep -v grep | cut -f1 -d"-" >> ./reports/usage/$1$2usage.csv
     fi
     if [[ $1 == "hivemq1" ]]; then
-        ps axo pid,etime,%cpu,%mem,cmd | grep 'hivemq' | grep -v grep | cut -f1 -d"-" >> ./reports/usage/hivemq$2usage.csv
+        ps axo pid,etime,%cpu,%mem,cmd | grep 'hivemq' | grep -v "./process_monitor.sh" | grep -v "grep" | grep -v "tee" | cut -f1 -d"-" >> ./reports/usage/hivemq$2usage.csv
     fi
 sleep 10
 done
