fe3ce8646d82d4bce349afa53c4b5bf38b59fc20
Jeremy <reaper9998@hotmail.com>
2021-03-26 12:15:59 -0400

    added cpu monitoring script

--- run_script.sh
+++ run_script.sh
@@ -8,18 +8,20 @@ numClients=("50" "500" "5000" "10000")
 echo "Stopping all existing brokers"
 sudo docker stop $(docker ps -a -q)
 for i in ${brokers[@]}; do
-	sleep 10
+	sleep 60
 	for t in ${numClients[@]}; do
-
-		echo "Starting $i Docker"
+		dt=$(date '+%d/%m/%Y %H:%M:%S');
+		echo "Starting $i Docker at $dt"
 		sudo docker start $i
 		echo "Waiting for broker to finish starting up"
 		sleep 10
+		timeout 900 ./process_monitor $3 &
 
 		echo "Running node script with $t publishers and $t subscribers with 0.04 messages/second for 15 minutes."
 		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=900 --limitLogging=1 | tee ./reports/$i$t.log
 
-		echo "Stopping $i Docker"
+		dt=$(date '+%d/%m/%Y %H:%M:%S');
+		echo "Stopping $i Docker at $dt"
 		sudo docker stop $i
 	done 
 done
