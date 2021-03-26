a42ff1553fde753c0ec23d527877aaab851a0add
Jeremy <reaper9998@hotmail.com>
2021-03-26 14:44:35 -0400

    temporarily removed timestamp

--- process_monitor.sh
+++ process_monitor.sh
@@ -7,12 +7,12 @@ if [[ "$#" -ne 2 ]]; then
 fi
 
 while true; do
-		dt=$(date '+%m/%d/%Y %H:%M:%S');
+	dt=$(date '+%m/%d/%Y %H:%M:%S');
     if [[ $1 == "emqx" ]] || [[ $1 == "vernemq" ]]; then
         ps axo pid,etime,%cpu,%mem,cmd | grep 'beam.smp' | grep -v grep | cut -f1 -d"-" >> ./reports/usage/$1$2usage.csv
     fi
     if [[ $1 == "hivemq1" ]]; then
-        ps axo pid,etime,%cpu,%mem,cmd | grep 'hivemq' | grep -v "./process_monitor.sh" | grep -v "grep" | grep -v "tee" | cut -f1 -d"-"  $dt >> ./reports/usage/hivemq$2usage.csv
+        ps axo pid,etime,%cpu,%mem,cmd | grep 'hivemq' | grep -v "./process_monitor.sh" | grep -v "grep" | grep -v "tee" | cut -f1 -d"-" >> ./reports/usage/hivemq$2usage.csv
     fi
 sleep 10
 done
