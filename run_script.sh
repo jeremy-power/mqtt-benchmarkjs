3c8ca13f544f3a4c7bda81b83244767cdcf2b9fc
Jeremy <reaper9998@hotmail.com>
2021-03-26 12:27:39 -0400

    added logging to all echo

--- run_script.sh
+++ run_script.sh
@@ -12,17 +12,18 @@ for i in ${brokers[@]}; do
 	sleep 60
 	for t in ${numClients[@]}; do
 		dt=$(date '+%d/%m/%Y %H:%M:%S');
-		echo "Starting $i Docker at $dt"
+		echo "Starting $i Docker at $dt" | tee -a ./reports/$i$t.log
 		sudo docker start $i
 		echo "Waiting for broker to finish starting up"
 		sleep 10
 		timeout 900 ./process_monitor $i $t &
 
-		echo "Running node script with $t publishers and $t subscribers with 0.04 messages/second for 15 minutes."
-		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=900 --limitLogging=1 | tee ./reports/$i$t.log
+		echo "Running node script on $i with $t publishers and $t subscribers with 0.04 messages/second for 15 minutes." | tee -a ./reports/$i$t.log
+		echo "Starting at $dt" | tee -a ./reports/$i$t.log
+		node mqtt-bm.js --host="127.0.0.1"  --numPubSub=$t --rate=0.04 --timeout=900 --limitLogging=1 | tee -a ./reports/$i$t.log
 
 		dt=$(date '+%d/%m/%Y %H:%M:%S');
-		echo "Stopping $i Docker at $dt"
+		echo "Stopping $i Docker at $dt" | tee -a ./reports/$i$t.log
 		sudo docker stop $i
 	done 
 done
